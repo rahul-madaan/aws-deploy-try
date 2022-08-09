1625becad55622a66ee0cb5e4a294f9c9b5a3aae
Rahul Madan <madan.rahul9@gmail.com>
2022-08-09 22:50:09 +0530

    added button to increase counter on click

---
+++ src/my_components/button.js
@@ -0,0 +1,17 @@
+import { useState} from "react";
+
+export const Button = () => {
+    const [button, setButton] = useState(0);
+    function handleClick() {
+        setButton(button+1);
+    }
+    return (
+        <>
+            <p>Number</p>
+            <>{button}</>
+            <div>
+                <button onClick={handleClick}>Click to increase number</button>
+            </div>
+        </>
+    )
+}
